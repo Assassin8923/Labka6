2b2593917b48b6e9b230e1153059b87a9ce91b02
Assassin8923 <c.hryniuk.rostyslav@student.uzhnu.edu.ua>
2026-02-19 01:30:58 +0200

    ÐЛамбораторна №7.1

--- labca/Program.cs
+++ labca/Program.cs
@@ -1,83 +1,10 @@
 ﻿using System.Text;
 
-namespace labca;
-
-public abstract class Ticket(string path, int prise)
-{
-    protected string? Path { get; set; } = path;
-    protected int Prise { get; set; } = prise;
-
-    public abstract void Buying();
-}
-
-public interface IRefuelable
-{
-    void Refill();
-}
-
-public class Economy(string path, int prise) : Ticket(path, prise), IRefuelable
-{
-    public override void Buying()
+    List<Action> actions = new List<Action>();
+    for (int i = 1; i <= 5; i++)
     {
-        Console.WriteLine($"Вартість проїзду за шляхом {Path},  {Prise} грн");
+        int copy = i;
+        actions.Add(() => Console.WriteLine($"{copy}"));
     }
 
-    public void Refill()
-    {
-        Console.WriteLine($"Квитків за шляхом:{Path} не залишилося");
-    }
-}
-
-public class Business(string path, int price) : Ticket(path, price), IRefuelable
-{
-    public override void Buying()
-    {
-        Console.WriteLine($"Вартість проїзду за шляхом {Path},  {Prise} грн");
-    }
-
-    public void Refill()
-    {
-        Console.WriteLine($"Квитків за шляхом:{Path} залишилося 20 штук");
-    }
-}
-
-public class FirstClass(string path, int prise) : Ticket(path, prise), IRefuelable
-{
-    public override void Buying()
-    {
-        Console.WriteLine($"Вартість проїзду за шляхом {Path},  {Prise} грн");
-    }
-
-    public void Refill()
-    {
-        Console.WriteLine($"Квитків за шляхом:{Path} залишилося 30");
-    }
-}
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        Console.InputEncoding = Encoding.UTF8;
-        
-        List<Ticket> tickets =
-        [
-            new Economy("Ужгород-Мукачево", 100),
-            new FirstClass("Київ-Харків", 5000),
-            new Business("Одеса-Львів", 1000)
-        ];
-
-        foreach (Ticket ticket in tickets)
-        {
-            ticket.Buying();
-
-            if (ticket is IRefuelable refuelable)
-            {
-                refuelable.Refill();
-            }
-
-            Console.WriteLine("---");
-        }
-    }
-}
+    foreach (var action in actions) action();
